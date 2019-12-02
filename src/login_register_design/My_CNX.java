c43465a3b2de17054b6a066a0b7e5f7acdb97d4f
unknown <nmochammad100@gmail.com>
2019-12-03 00:15:14 +0700

    java

---
+++ src/login_register_design/My_CNX.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login_register_design;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Sir
+ */
+public class My_CNX {
+    
+    private static String servername   = "localhost";
+    private static String username     = "root";
+    private static String password     = "adminweb123";
+    private static String dbname       = "users_db";
+    private static Integer portnumber   = 3306;
+    
+    public static Connection getConnection() {
+        
+        Connection cnx = null;
+        MysqlDataSource datasource = new MysqlDataSource();
+        
+        datasource.setServerName(servername);
+        datasource.setUser(username);
+        datasource.setPassword(password);
+        datasource.setDatabaseName(dbname);
+        datasource.setPortNumber(portnumber);
+        
+        try {
+            cnx = (Connection) datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(" Get Connection -> " + My_CNX.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return cnx;
+        
+    }
+    
+}
